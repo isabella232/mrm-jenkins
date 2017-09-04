be311d884b990d336fa59f069f0274d18d9de0c0
Timofey Turenko <timofey.turenko@skysql.com>
2017-09-04 17:33:07 +0300

    add setup replication scripts

--- run_test.sh
+++ run_test.sh
@@ -13,10 +13,30 @@ export value=`echo $value | sed "s/?//g"`
 export dir=`pwd`
 export repo_dir=$dir/repo.d/
 
+echo "Bringing up VMa"
 ~/mrm-jenkins/create_config.sh
 res=$?
 
+echo "Loading VMs parameters to variables"
+set -a
+. $MDBCI_VM_PATH/$name.network_config
+set +a
 
+echo "Setting up replication"
+~/mrm-jenkins/setup_repl/setup_repl.sh
+
+echo "Generating maxscale.cnf"
+cp ~/mdbci-jenkins/cnf/replication.cnf maxscale.cnf.tmp
+eval "cat <<EOF
+$(maxscale.cnf.tmp)
+EOF" > maxscale.cnf
+
+export scpopt="-i $maxscale_keyfile -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ConnectTimeout=120 "
+export sshopt="$scpopt $maxscale_whoami@$maxscale_network"
+
+echo "Copying maxscale.cnf to VM"
+scp $scpopt maxscale.cnf $maxscale_whoami@$maxscale_network:~/
+$sshopt 'sudo cp ~/maxscale.cnf /etc/'
 
 cd $MDBCI_VM_PATH/$name
 if [ "$do_not_destroy_vm" != "yes" ] ; then
---
+++ setup_repl/setup_repl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+export node_N=4
+
+x=`expr $node_N - 1`
+for i in $(seq 0 $x)
+do
+        num=`printf "%03d" $i`
+        sshkey_var=node_"$num"_keyfile
+        user_var=node_"$num"_whoami
+        IP_var=node_"$num"_network
+
+        sshkey=${!sshkey_var}
+        user=${!user_var}
+        IP=${!IP_var}
+
+	ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP "sudo service mysql stop" 
+	sleep 5
+	ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP 'sudo sed -i "s/bind-address/#bind-address/g" /etc/mysql/my.cnf'
+	ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP 'sudo ln -s /etc/apparmor.d/usr.sbin.mysqld /etc/apparmor.d/disable/usr.sbin.mysqld; sudo service apparmor restart'
+
+	ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP "sudo mysql_install_db; sudo chown -R mysql:mysql /var/lib/mysql"
+	ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP "sudo service mysql start" 
+
+	sleep 15
+        scp -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $scr_dir/create_*_user.sql $user@$IP://home/$user/
+        ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP "sudo mysql < /home/$user/create_repl_user.sql"
+        ssh -i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$IP "sudo mysql < /home/$user/create_skysql_user.sql"
+done
+
+
+for i in $(seq 1 $x)
+do
+        num=`printf "%03d" $i`
+        IP_var=node_"$num"_network
+        IP=${!IP_var}
+
+	echo "CHANGE MASTER TO  MASTER_HOST=$node_000_network, MASTER_USER='repl',MASTER_PASSWORD='repl',MASTER_PORT=3306,MASTER_USE_GTID=Slave_pos;" | mysql -uskysql -pskysql -P 3306 -h $IP 
+done
