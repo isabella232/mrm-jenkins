07c034e96452715d7cd5454af348d4e32581f314
Timofey Turenko <timofey.turenko@skysql.com>
2017-09-04 12:04:01 +0300

    add first run_test

---
+++ create_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -x
+cd $dir
+export MDBCI_VM_PATH=$HOME/vms; mkdir -p $MDBCI_VM_PATH
+~/mdbci/repository-config/generate_all.sh repo.d
+~/mdbci/repository-config/maxscale-ci.sh $maxscale_target repo.d $ci_url_suffix
+
+export repo_dir=$dir/repo.d/
+
+echo "box: $box"
+echo "template: $template"
+
+provider=`$HOME/mdbci/mdbci show provider $box --silent 2> /dev/null`
+template_raw="$HOME/mrm-jenkins/template.$provider.json"
+eval "cat <<EOF
+$(<x.cnf)
+EOF" > "$MDBCI_VM_PATH/$name.json"
+
+mkdir -p $MDBCI_VM_PATH/$name
+cd $MDBCI_VM_PATH/$name
+vagrant destroy -f
+cd $dir
+
+$HOME/mdbci/mdbci --override --template  $MDBCI_VM_PATH/$name.json --repo-dir $repo_dir generate $name
+
+while [ -f ~/vagrant_lock ]
+do
+	echo "vagrant is locked, waiting ..."
+	sleep 5
+done
+touch ~/vagrant_lock
+echo $JOB_NAME-$BUILD_NUMBER >> ~/vagrant_lock
+
+echo "running vagrant up $provider"
+
+$HOME/mdbci/mdbci up $name --attempts 3
+if [ $? != 0 ]; then
+	echo "Error creating configuration"
+	exit 1
+fi
+
+cp ~/build-scripts/team_keys .
+$HOME/mdbci/mdbci  public_keys --key team_keys $name
---
+++ run_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# $1 - "debug" means do not install Maxscale
+ulimit -n
+rm -rf LOGS
+export MDBCI_VM_PATH=$HOME/vms; mkdir -p $MDBCI_VM_PATH
+export target=`echo $target | sed "s/?//g"`
+export name=`echo $name | sed "s/?//g"`
+export value=`echo $value | sed "s/?//g"`
+
+. ~/build-scripts/test/configure_log_dir.sh
+
+export dir=`pwd`
+export repo_dir=$dir/repo.d/
+
+~/build-scripts/test/create_config.sh
+res=$?
+
+
+
+cd $MDBCI_VM_PATH/$name
+if [ "$do_not_destroy_vm" != "yes" ] ; then
+	vagrant destroy -f
+        rm ~/vagrant_lock
+	echo "clean  up done!"
+fi
+cd $dir
+exit $res
