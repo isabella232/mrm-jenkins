ba463b957f84d4659fc0e7e286ff763ef53055a1
Timofey Turenko <timofey.turenko@skysql.com>
2017-09-13 11:34:38 +0300

    temparal fix of binary copying

--- remote_build_mrm.sh
+++ remote_build_mrm.sh
@@ -70,10 +70,11 @@ fi
 
 echo "copying binaries"
 mkdir -p ~/repository/$target/$image/mrm/
-scp $scpopt $sshuser@$IP:./go/bin/* ~/repository/$target/$image/mrm/
-scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.gz ~/repository/$target/mrm/$image/
-scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.deb ~/repository/$target/mrm/$image/
-scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.rpm ~/repository/$target/mrm/$image/
+#scp $scpopt $sshuser@$IP:./go/bin/* ~/repository/$target/$image/mrm/
+#scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.gz ~/repository/$target/mrm/$image/
+#scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.deb ~/repository/$target/mrm/$image/
+#scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/*.rpm ~/repository/$target/mrm/$image/
+scp $scpopt $sshuser@$IP:./go/src/github.com/mariadb-corporation/mrm/mrm ~/repository/$target/mrm/$image/
 
 echo "package building for $target done!"
 
